09a0be3dc56bcfdf81ce1353c7798206ba4abe2c
elliottrottier <elliottrottier@hunschool.org>
2026-04-13 10:27:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const card = document.querySelector(".container");
+document.addEventListener("DOMContentLoaded", () => {
+    const card = document.querySelector(".container");
 
-document.addEventListener("mousemove", (e) => {
-    let x = (window.innerWidth / 2 - e.clientX) / 25;
-    let y = (window.innerHeight / 2 - e.clientY) / 25;
+    if (!card) return;
 
-    card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+    document.addEventListener("mousemove", (e) => {
+        let x = (window.innerWidth / 2 - e.clientX) / 25;
+        let y = (window.innerHeight / 2 - e.clientY) / 25;
+
+        card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+    });
 });
